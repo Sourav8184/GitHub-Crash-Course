555a87e2f28c8ec401dbca21c0ae7eb6d7cd96c8
Sourav <ksourav662@gmail.com>
2024-05-26 02:25:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello Git and Github");
+const age = 24;
+console.log("git init");
+console.log("git add <file_Name>");
+console.log("(git add .) add all the files in the staging area");
+console.log("git diff");
---
+++ math.js
@@ -0,0 +1,2 @@
+console.log("this file is untracked:");
+console.log("first to run (git add .) command to track this file:");

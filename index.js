51cbe4e0514ae77664185778510fb8d05daa6834
Sourav <ksourav662@gmail.com>
2024-05-26 02:42:17 +0530

    add some functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("Hello Git and Github");
-const age = 24;
+const age = 25;
 console.log("git init");
 console.log("git add <file_Name>");
 console.log("(git add .) add all the files in the staging area");
--- math.js
+++ math.js
@@ -1,2 +1,16 @@
 console.log("this file is untracked:");
 console.log("first to run (git add .) command to track this file:");
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function sub(num1, num2) {
+  if (num1 > num2) {
+    console.log(num1 - num2);
+  } else {
+    console.log(num2 - num1);
+  }
+}
+
+export { add, sub };
